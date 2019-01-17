c93734a6089e1525c7e8beb257719234d4f83bb4
AlexDNZ <paleozene@gmail.com>
2019-01-17 18:36:36 +0100

    second commit

--- lib/depute_scrapper.rb
+++ lib/depute_scrapper.rb
@@ -2,27 +2,73 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-annuaire_depute = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/liste/alphabetique"))
+list_depute = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/liste/alphabetique"))
 
-#def get_name_depute(annuaire)
-
+def get_name_depute(annuaire)
 name_depute =[]
-annuaire_depute.xpath('/html/body/div[3]/div/div/section/div/article/div[3]/div/div/div/ul//li/a').each do |e|
-  if e.content.start_with?("M.")
-    name_depute << e.content[3..-1]
-  elsif e.content.start_with?("Mme")
-    name_depute << e.content[4..-1]
-  else
-    name_depute << e.content
+  annuaire.xpath('/html/body/div[3]/div/div/section/div/article/div[3]/div/div/div/ul//li/a').each do |e|
+    if e.content.start_with?("M.")
+      name_depute << e.content[3..-1]
+    elsif e.content.start_with?("Mme")
+      name_depute << e.content[4..-1]
+    else
+      name_depute << e.content
+    end
   end
-  puts name_depute
+  return name_depute
 end
 
+#puts get_name_depute(list_depute).inspect
+
+final_arr = []
+get_name_depute(list_depute).each do |e|
+  arr = e.split
+  first_name = arr[0]
+  last_name = ""
+  mailbegin= "#{first_name}."
+  arr[1..-1].each do |i|
+    last_name << i
+    last_name << " "
+    mailbegin << i
+  end
+  final_arr << { "first_name" => "#{first_name}",
+  	             "last_name" => "#{last_name[0..-2]}",
+                  "email" => "#{mailbegin.downcase}@assemblee-nationale.fr" , }
+end
+
+puts final_arr.inspect
+
+# id_card = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/fiche/OMC_PA720568"))
+# mail = id_card.xpath('/html/body/div[3]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[4]/ul/li[1]/a').attr('href')
+# mail = mail.to_s[7..-1]
+# puts mail
 
 
+# def get_depute_url(annuaire)
+#   depute_list_url =[]
+#   annuaire.xpath('/html/body/div[3]/div/div/section/div/article/div[3]/div/div/div/ul//li/a').each do |el|
+#   url = "http://www2.assemblee-nationale.fr/#{el.attr('href')[1..-1]}"
+#   depute_list_url << url
+#   end
+#   return depute_list_url
+# end
 
+#puts get_depute_url(list_depute)
 
+# def get_mail_depute(annuaire)
+#   puts "Veuillez patienter pendant le scrapping ..."
+#   depute_list_mail =[]
+#   i=0
+#   get_depute_url(annuaire).each do |e|
+#     adr = Nokogiri::HTML(open(e))
+#     email = adr.xpath.xpath('/html/body/div[3]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[4]/ul/li[1]/a').attr('href')
+#     email = email.to_s[7..-1]
+#     depute_list_mail << email
+#     i+=1
+#     puts "#{i} mails chargés"
+#     puts email
+#   end
+#   return depute_list_mail
+# end
 
-# /html/body/div[3]/div/div/section/div/article/div[3]/div/div[3]/div[6]/ul[1]/li[2]/a
-#
-# /html/body/div[3]/div/div/section/div/article/div[3]/div/div[3]/div[6]/ul[1]/li[3]/a
+#puts get_mail_depute(list_depute)
--- lib/townhall_scrapper.rb
+++ lib/townhall_scrapper.rb
@@ -2,13 +2,13 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-page_mairie = Nokogiri::HTML(open('http://annuaire-des-mairies.com/95/arronville.html'))
+page_mairie_arronville = Nokogiri::HTML(open('http://annuaire-des-mairies.com/95/arronville.html'))
 
 def get_townhall_email(townhall_url)
   return townhall_url.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
 end
 
-#puts get_townhall_email(page_mairie)
+#puts get_townhall_email(page_mairie_arronville)
 
 annuaire_valdoise = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
 
